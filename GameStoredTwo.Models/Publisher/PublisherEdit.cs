be0cdb1d534da727756426b42cef13c0b4f57dcd
dfdevaney <dfdevaney@gmail.com>
2020-11-25 14:03:53 -0600

    File System Setup

---
+++ GameStoredTwo.Models/Publisher/PublisherEdit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStoredTwo.Models.Publisher
+{
+    public class PublisherEdit
+    {
+
+    }
+}
---
+++ GameStoredTwo.Models/User/UserCreate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStoredTwo.Models.User
+{
+    public class UserCreate
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string State { get; set; }
+    }
+}
